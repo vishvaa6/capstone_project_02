97233c614f3d6e588fe86e85ab46530b72d4e3c6
vishvaa <vishvaav006@gmailcom>
2024-10-30 17:55:11 +0530

    Project_2

--- Project_2/test_main.py
+++ Project_2/test_main.py
@@ -11,108 +11,108 @@ def test_start():
 
 def test_forgot_password():
     assert ForgotPassword().forgot_password() == True
-    print("SUCCESS : -------------- The Username Box is visible --------------")
+    print("TEST_CASE_01 : SUCCESS : The Username Box is visible")
 
 def test_reset_pass_notification():
     assert ForgotPassword().reset_pass_notification() == "Reset Password link sent successfully"
-    print("SUCCESS : -------------- Reset Password link sent successfully --------------")
+    print("TEST_CASE_01 : SUCCESS : Reset Password link sent successfully ")
 
 #---------------------------- TEST_CASE_02-----------------------------#
 
 def test_start2():
     assert StartStop().start() == True
-    print("automation started")
+    print("TEST_CASE_02 : automation started")
 
 def test_log():
     assert HeaderValidation().login() == True
-    print("Automation started successfully")
+    print("TEST_CASE_02 : Login Successfully")
 
 def test_head():
     assert HeaderValidation().title() == "OrangeHRM"
-    print("Title Validation is successful")
+    print("TEST_CASE_02 : Title Validation is successful")
 
 def test_user_management():
     assert HeaderValidation().user_manage() == True
-    print("User Management Tab is Visible")
+    print("TEST_CASE_02 : User Management Tab is Visible")
 
 def test_job_tab():
     assert HeaderValidation().job_tab() == True
-    print("job Tab is Visible")
+    print("TEST_CASE_02 : job Tab is Visible")
 
 def test_organization_tab():
     assert HeaderValidation().organization_tab() == True
-    print("organization Tab is Visible")
+    print("TEST_CASE_02 : organization Tab is Visible")
 
 def test_qualification_tab():
     assert HeaderValidation().qualification_tab() == True
-    print("Qualification Tab is Visible")
+    print("TEST_CASE_02 : Qualification Tab is Visible")
 
 def test_nationalities_tab():
     assert HeaderValidation().nationalities_tab() == True
-    print("nationalities Tab is Visible")
+    print("TEST_CASE_02 : nationalities Tab is Visible")
 
 def test_corporate_branding_tab():
     assert HeaderValidation().corporate_branding_tab() == True
-    print("corporate_branding Tab is Visible")
+    print("TEST_CASE_02 : corporate_branding Tab is Visible")
 
 def test_configuration_tab():
     assert HeaderValidation().configuration_tab() == True
-    print("configuration Tab is Visible")
+    print("TEST_CASE_02 : configuration Tab is Visible")
 
 #---------------------------- TEST_CASE_03-----------------------------#
 
 def test_admin():
     assert MenuValidation().admin() == 'Admin'
-    print("SUCCESS : Admin tab is visible")
+    print("TEST_CASE_03 : SUCCESS : Admin tab is visible")
 
 def test_pim():
     assert MenuValidation().pim() == 'PIM'
-    print("SUCCESS : PIM tab is visible")
+    print("TEST_CASE_03 : SUCCESS : PIM tab is visible")
 
 
 def test_leave():
     assert MenuValidation().leave() == 'Leave'
-    print("SUCCESS : lEAVE tab is visible")
+    print("TEST_CASE_03 : SUCCESS : lEAVE tab is visible")
 
 
 def test_time():
     assert MenuValidation().time() == 'Time'
-    print("SUCCESS : TIME tab is visible")
+    print("TEST_CASE_03 : SUCCESS : TIME tab is visible")
 
 
 def test_recruitment():
     assert MenuValidation().recruitment() == 'Recruitment'
-    print("SUCCESS : RECRUITMENT tab is visible")
+    print("TEST_CASE_03 : SUCCESS : RECRUITMENT tab is visible")
 
 
 def test_my_info():
     assert MenuValidation().my_info() == 'My Info'
-    print("SUCCESS : MY INFO tab is visible")
+    print("TEST_CASE_03 : SUCCESS : MY INFO tab is visible")
 
 
 def test_performance():
     assert MenuValidation().performance() == 'Performance'
-    print("SUCCESS : PERFORMANCE tab is visible")
+    print("TEST_CASE_03 : SUCCESS : PERFORMANCE tab is visible")
 
 
 def test_dashboard():
     assert MenuValidation().dashboard() == 'Dashboard'
-    print("SUCCESS : DASHBOARD tab is visible")
+    print("TEST_CASE_03 : SUCCESS : DASHBOARD tab is visible")
 
 
 def test_directory():
     assert MenuValidation().directory() == 'Directory'
-    print("SUCCESS : DIRECTORY tab is visible")
+    print("TEST_CASE_03 : SUCCESS : DIRECTORY tab is visible")
 
 
 def test_maintenance():
     assert MenuValidation().maintenance() == 'Maintenance'
-    print("SUCCESS : MAINTENANCE tab is visible")
+    print("TEST_CASE_03 : SUCCESS : MAINTENANCE tab is visible")
 
 def test_buzz():
     assert MenuValidation().buzz() == 'Buzz'
-    print("SUCCESS : BUZZ tab is visible")
+    print("TEST_CASE_03 : SUCCESS : BUZZ tab is visible")
 
 def test_quit3():
     assert StartStop().quit() == True
-    print("Automation Stopped")
+    print("TEST_CASE_03 : Automation Stopped")
