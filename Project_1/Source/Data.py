ecbecc68aa7777afd40ba8dd7c3e61a3cdfe6701
vishvaa6 <vishvaav006@gmail.com>
2024-11-01 09:32:11 +0530

    Delete Project_1 directory

--- Project_1/Source/Data.py
+++
@@ -1,4 +0,0 @@
-class Data:
-    url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"   # testing url
-    username = "Admin"                                                           # user_input
-    password = ["admin123","invalid password"]                                   # user_password
